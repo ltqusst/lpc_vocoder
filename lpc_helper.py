57705df032ac5c6db2024b384eeecb046d304895
Li, Tingqian <tingqian.li@intel.com>
2022-01-17 14:37:57 +0800

    Update STFT

--- lpc_helper.py
+++ lpc_helper.py
@@ -5,47 +5,48 @@ import IPython
 import matplotlib.pyplot as plt
 from vscode_audio import Audio
 
-def play_sound(sound, rate=44100, name="", spectrum = 0):
+def play_sound(sound, rate=44100, name="", spectrum = 0, waveform = 1):
     """Play a mono 44Khz sound file in the browser"""
     #return IPython.display.display(IPython.display.Audio(sound,rate=rate))
-    plt.figure(figsize=(32,8), dpi= 100, facecolor='w', edgecolor='k')
+    if waveform > 0:
+        plt.figure(figsize=(32,8), dpi= 100, facecolor='w', edgecolor='k')
 
-    if (isinstance(sound, list)):
-        N = len(sound)
-        for i in range(N):
-            plt.subplot(N, 1, i+1)
-            times = np.arange(0, len(sound[i])/44100, 1/44100)
-            plt.plot(times, sound[i], 'k-')
-            plt.title(name)
+        if (isinstance(sound, list)):
+            N = len(sound)
+            for i in range(N):
+                plt.subplot(N, 1, i+1)
+                times = np.arange(0, len(sound[i])/44100, 1/44100)
+                plt.plot(times, sound[i], 'k-')
+                plt.title(name)
+            plt.show()
+            return IPython.display.display(Audio(np.hstack(sound),sr=rate, name=name))
+
+        if spectrum >= 0:
+            plt.subplot(211)
+        times = np.arange(0, len(sound)/44100, 1/44100)
+        plt.plot(times, sound, 'k-')
+        plt.xlabel("Time (s)")
+        plt.ylabel("sample")
+
+        if spectrum == 1:
+            plt.subplot(212)
+            powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(sound, Fs=rate, scale='dB')
+            plt.xlabel('Time')
+            plt.ylabel('Frequency')
+
+        if spectrum == 0:
+            fourier_transform = np.fft.rfft(sound)
+            power_spectrum = 20 * np.log10(np.abs(fourier_transform))
+            frequency = np.linspace(0, rate/2, len(power_spectrum))
+            plt.subplot(212)
+            plt.plot(frequency, power_spectrum)
+            plt.xlim(0,4000)
+            plt.ylim(bottom=0)
+            plt.xlabel('Frequency')
+            plt.ylabel('power(dB)')
+
+        plt.title(name)
         plt.show()
-        return IPython.display.display(Audio(np.hstack(sound),sr=rate, name=name))
-
-    if spectrum >= 0:
-        plt.subplot(211)
-    times = np.arange(0, len(sound)/44100, 1/44100)
-    plt.plot(times, sound, 'k-')
-    plt.xlabel("Time (s)")
-    plt.ylabel("sample")
-
-    if spectrum == 1:
-        plt.subplot(212)
-        powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(sound, Fs=rate, scale='dB')
-        plt.xlabel('Time')
-        plt.ylabel('Frequency')
-
-    if spectrum == 0:
-        fourier_transform = np.fft.rfft(sound)
-        power_spectrum = 20 * np.log10(np.abs(fourier_transform))
-        frequency = np.linspace(0, rate/2, len(power_spectrum))
-        plt.subplot(212)
-        plt.plot(frequency, power_spectrum)
-        plt.xlim(0,4000)
-        plt.ylim(bottom=0)
-        plt.xlabel('Frequency')
-        plt.ylabel('power(dB)')
-
-    plt.title(name)
-    plt.show()
     return IPython.display.display(Audio(sound,sr=rate, name=name))
 
 def load_wave(fname):
